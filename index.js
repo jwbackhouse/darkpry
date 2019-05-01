d066e608704b5233d5e840726649364251e1466c
James Wood <jamesjwood@msn.com>
2019-05-01 22:51:42 +0100

    split out asyncForEach into a seperate file

--- index.js
+++ index.js
@@ -2,35 +2,24 @@
 //https://api.darksky.net/forecast/d3cdf47118616664be90631b60bbc0a2/42.3601,-71.0589
 const fetch = require('node-fetch');
 const fs = require('fs');
-
-const DARK_SKY_TOKEN = process.env.DARK_SKY_TOKEN;
-if(!DARK_SKY_TOKEN) {throw new Error("The environment variable DARK_SKY_TOKEN must be set")}
-
-const path = `/forecast/${DARK_SKY_TOKEN}/`
-
-let myCoordsData;
-if(fs.existsSync('input.json')) {
-	console.log("Found an input.json file, using this")
-	myCoordsData = fs.readFileSync('input.json'); 
-} else {
-	console.log("Did not find an input.json file, using sampleInput.json")
-	myCoordsData = fs.readFileSync('sampleInput.json'); 
-}
-
-const myCoords = JSON.parse(myCoordsData);
-
-const returnedData = [];
-
-
-async function asyncForEach(array, callback) {
-	for (let index = 0; index < array.length; index++) {
-	  await callback(array[index], index, array);
-	}
-}
-
+const asyncForEach = require('./asyncForEach');
 
 const main = async () => {
-	await asyncForEach(myCoords, async (coordinate)=> {
+	const DARK_SKY_TOKEN = process.env.DARK_SKY_TOKEN;
+	if (!DARK_SKY_TOKEN) { throw new Error("The environment variable DARK_SKY_TOKEN must be set") }
+	const path = `/forecast/${DARK_SKY_TOKEN}/`
+	let myCoordsData;
+	if (fs.existsSync('input.json')) {
+		console.log("Found an input.json file, using this")
+		myCoordsData = fs.readFileSync('input.json');
+	} else {
+		console.log("Did not find an input.json file, using sampleInput.json")
+		myCoordsData = fs.readFileSync('sampleInput.json');
+	}
+	const myCoords = JSON.parse(myCoordsData);
+
+	const returnedData = [];
+	await asyncForEach(myCoords, async (coordinate) => {
 		console.log("Getting data for lat: " + coordinate.lat + " long: " + coordinate.long);
 		const options = {
 			hostname: 'api.darksky.net',
@@ -43,12 +32,12 @@ const main = async () => {
 		returnedData.push(json);
 	});
 
-	let data = JSON.stringify(returnedData);  
-	fs.writeFileSync('output.json', data); 
+	let data = JSON.stringify(returnedData);
+	fs.writeFileSync('output.json', data);
 	console.log("Data saved to output.json");
 }
 
-main().catch((error)=> {
+main().catch((error) => {
 	console.log("There was an error");
 	console.error(error);
 })
